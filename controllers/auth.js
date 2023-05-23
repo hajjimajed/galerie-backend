9a300a63101c9ddf46fa949acbcfafe63a896913
hajjimajed <hajjimajed78@gmail.com>
2023-05-23 23:27:15 +0100

    creating login and get user data routes and controllers and jwt middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator')
-
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
 
@@ -13,14 +13,17 @@ exports.signup = (req, res, next) => {
     const profileImg = req.body.profileImg;
     const role = req.body.role;
 
-    const user = new User({
-        email: email,
-        password: password,
-        name: name,
-        profileImg: profileImg,
-        role: role
-    })
-    user.save()
+    bcrypt.hash(password, 12)
+        .then(hashedPassword => {
+            const user = new User({
+                email: email,
+                password: hashedPassword,
+                name: name,
+                profileImg: profileImg,
+                role: role
+            })
+            return user.save()
+        })
         .then(result => {
             res.status(201).json({ message: 'user created', userId: result._id })
         })
@@ -30,5 +33,67 @@ exports.signup = (req, res, next) => {
             }
             next(err);
         })
+}
 
+
+exports.login = (req, res, next) => {
+
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                const error = new Error('user not exist');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual => {
+            if (!isEqual) {
+                const error = new Error('Wrong password');
+                error.statusCode = 401;
+                throw error;
+            }
+
+            const token = jwt.sign(
+                {
+                    email: loadedUser.email,
+                    userId: loadedUser._id.toString()
+                },
+                'secretcodegenerator',
+                { expiresIn: '2h' }
+            )
+            res.status(201).json({ token: token, userId: loadedUser._id.toString() })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+
+}
+
+exports.userData = (req, res, next) => {
+    const userId = req.userId;
+
+    User.findById(userId)
+        .then(user => {
+            if (!user) {
+                const error = new Error('User not found');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({ message: 'data retrieved', user: user })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 }
--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,17 @@ const express = require('express');
 
 const authController = require('../controllers/auth');
 
+const isAuth = require('../middleware/is-auth')
+
 const router = express.Router();
 
 const { body } = require('express-validator');
 
 
 router.put('/signup', authController.signup);
+
+router.post('/login', authController.login);
+
+router.get('/user-data', isAuth, authController.userData);
 
 module.exports = router;
